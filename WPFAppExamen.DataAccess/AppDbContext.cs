d0e206351e9abb84b5cbea47f1bd77457d6d2e3f
Andreas Otten <andreas.otten@atlascopco.com>
2021-06-01 17:16:13 +0200

    Add project files.

---
+++ WPFAppExamen.DataAccess/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WPFAppExamen.Model;
+
+namespace WPFAppExamen.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base()
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=AIRLBEOF11958\\SYNTRA;Initial Catalog=WPF;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+           
+
+        }
+    }
+}
---
+++ WPFAppExamen.Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WPFAppExamen.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+    }
+}
